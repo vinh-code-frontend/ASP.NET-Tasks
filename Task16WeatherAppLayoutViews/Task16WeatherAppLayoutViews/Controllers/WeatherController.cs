b0923aff30a3d006a18ce1d422ea3d51e8092a54
Nguyen Le Vinh <vikvenus199x@gmail.com>
2024-07-21 23:04:18 +0700

    [ASP.NET] Task 16: Weather App with Layout Views

---
+++ Task16WeatherAppLayoutViews/Task16WeatherAppLayoutViews/Controllers/WeatherController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task16WeatherAppLayoutViews.Models;
+
+namespace Task16WeatherAppLayoutViews.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private List<CityWeather> cityWeathers = new List<CityWeather>()
+        {
+            new CityWeather ()
+            {
+               CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),  TemperatureFahrenheit = 33
+            },
+            new CityWeather ()
+            {
+                CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),  TemperatureFahrenheit = 60
+            },
+            new CityWeather()
+            {
+                CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  TemperatureFahrenheit = 82
+            }
+        };
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(cityWeathers);
+        }
+        [Route("/weather/{cityCode?}")]
+        public IActionResult CityWeather(string? cityCode)
+        {
+            ViewData["Title"] = "Can't find city";
+            if (cityCode == null)
+            {
+                return View();
+            }
+            CityWeather? matchingCity = cityWeathers.Where(city => city.CityUniqueCode == cityCode).FirstOrDefault();
+            if (matchingCity != null)
+            {
+                ViewData["Title"] = $"Weather of {matchingCity.CityName}";
+            }
+            return View(matchingCity);
+        }
+    }
+}
