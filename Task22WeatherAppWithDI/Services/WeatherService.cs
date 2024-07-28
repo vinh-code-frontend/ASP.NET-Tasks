849e456b9e9df69a1a765c68b4121b7aaf3b7bcc
Nguyen Le Vinh <vikvenus199x@gmail.com>
2024-07-28 21:40:06 +0700

    [ASP.NET] Task 22: Weather app with Dependency Injection

---
+++ Task22WeatherAppWithDI/Services/WeatherService.cs
@@ -0,0 +1,44 @@
+﻿using Models;
+using ServiceContracts;
+
+namespace Services
+{
+    public class WeatherService : IWeatherService
+    {
+        private readonly List<CityWeather> _cityWeather;
+        public WeatherService()
+        {
+            _cityWeather = new List<CityWeather>() {
+                new CityWeather()
+                {
+                    CityUniqueCode = "LDN",
+                    CityName = "London",
+                    DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),
+                    TemperatureFahrenheit = 33
+                },
+                new CityWeather(){
+                    CityUniqueCode = "NYC",
+                    CityName = "London",
+                    DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),
+                    TemperatureFahrenheit = 60
+                },
+                new CityWeather()
+                {
+                    CityUniqueCode = "PAR",
+                    CityName = "Paris",
+                    DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),
+                    TemperatureFahrenheit = 82
+                }
+            };
+        }
+        public List<CityWeather> GetWeatherDetails()
+        {
+            return _cityWeather;
+        }
+        public CityWeather? GetWeatherByCityCode(string CityCode)
+        {
+            CityWeather? city = _cityWeather.FirstOrDefault(item => item.CityUniqueCode == CityCode);
+            return city;
+        }
+    }
+}
---
+++ Task22WeatherAppWithDI/Task22WeatherAppWithDI/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using ServiceContracts;
+
+namespace Task22WeatherAppWithDI.Controllers
+{
+    [Controller]
+    public class HomeController : Controller
+    {
+        private readonly IWeatherService _weatherService;
+        public HomeController(IWeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Cities Weather";
+            List<CityWeather> weatherList = _weatherService.GetWeatherDetails();
+            return View(weatherList);
+        }
+        [Route("/weather/{cityCode}")]
+        public IActionResult WeatherDetails(string? cityCode)
+        {
+            ViewData["Title"] = "Can't find city";
+            if (cityCode == null)
+            {
+                return View();
+            }
+            CityWeather? matchingCity = _weatherService.GetWeatherByCityCode(cityCode);
+            if (matchingCity != null)
+            {
+                ViewData["Title"] = $"Weather of {matchingCity.CityName}";
+            }
+            return View(matchingCity);
+        }
+    }
+}
