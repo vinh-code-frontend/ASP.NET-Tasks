1105acfbb1be6d2efa8375928a946ac567338184
Nguyen Le Vinh <vikvenus199x@gmail.com>
2024-08-01 22:31:03 +0700

    [ASP.NET] Task 24: Social Media Links

---
+++ Task24SocialMediaLinks/Task24SocialMediaLinks/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Task24SocialMediaLinks.Models;
+
+namespace Task24SocialMediaLinks.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SocialMediaLinksOptions _socialMediaLinkOptions;
+        public HomeController(IOptions<SocialMediaLinksOptions> socialMediaLinkOptions)
+        {
+            _socialMediaLinkOptions = socialMediaLinkOptions.Value;
+        }
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(_socialMediaLinkOptions);
+        }
+    }
+}
---
+++ Task24SocialMediaLinks/Task24SocialMediaLinks/Models/SocialMediaLinksOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Task24SocialMediaLinks.Models
+{
+    public class SocialMediaLinksOptions
+    {
+        public string? Facebook { get; set; }
+        public string? Instagram { get; set; }
+        public string? Twitter { get; set; }
+        public string? Youtube { get; set; }
+    }
+}
